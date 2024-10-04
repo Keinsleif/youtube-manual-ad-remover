a1a86b881a06efb8ce915aa77df549100d6f10a4
Keinsleif <mountaindull@gmail.com>
2024-10-04 12:30:38 +0900

    update src/extension.js

--- src/extension.js
+++ src/extension.js
@@ -2,14 +2,17 @@ window.addEventListener("keydown", function (e) {
     if ((e.key == "Esc" || e.key == "Escape") || (e.shiftKey && e.key == "E")) {
         elements = document.querySelector("button[class*=ad-skip]");
         if (elements) {
+            elements.focus();
             elements.click();
         }
         elements = document.querySelector("button[class*=skip-ad]");
         if (elements) {
+            elements.focus();
             elements.click();
         }
         elements = document.querySelector("button[class*=ad-overlay-close]");
         if (elements) {
+            elements.focus();
             elements.click();
         }
     }
